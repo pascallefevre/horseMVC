40faf44dc27e363271cf397654d0b0608a8a3843
pascallefevre <pascal.lefevre.frde@gmail.com>
2019-10-03 08:39:27 +0200

    ajout de l'image tentative de push

--- src/main/java/com/horses/config/WebConfiguration.java
+++ src/main/java/com/horses/config/WebConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
@@ -24,4 +25,9 @@ public class WebConfiguration implements WebMvcConfigurer
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
+	@Bean
+	 public void addResourceHandlers(ResourceHandlerRegistry registry) { 
+	       
+	        registry.addResourceHandler("/images/pferd.jpg").addResourceLocations("/images/");
+	    }
 }
